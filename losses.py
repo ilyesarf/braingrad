051b865ca2b58901745991e6da24a69c4dfa8083
Quimzy <bam.boy2020@gmail.com>
2023-02-16 00:37:17 +0300

    losses: implemented mean squared error

--- losses.py
+++ losses.py
@@ -5,7 +5,7 @@ def cross_entropy(yHat, y):
     '''
     Calculates the cross entropy between two prbability distributions
 
-    Formula: CE(yHat, y) = -1/N * sum(y * log(yHat))
+    Formula: CE(yHat, y) = -1/N * Σ[i=1; N] y,i * log(yHat,i)
 
     Args:
         yHat (ndarray (n,)) : estimations
@@ -13,3 +13,16 @@ def cross_entropy(yHat, y):
     '''
     loss = -np.sum(y*np.log(yHat))
     return loss / float(yHat.shape[0]) #normalize loss over samples 
+
+def mean_squared_error(yHat, y):
+    '''
+    Calculates the mean squared error
+
+    Formula: MSE(yHat, y) = 1/N * Σ[i=1; N] (y,i - yHat,i) 
+
+    Args:
+        yHat (ndarray (n,)) : estimations
+        y (ndarray (n,)) : target values
+    '''
+    return np.square(np.subtract(y, yHat)).mean()
+
