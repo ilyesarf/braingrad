6969a0992fc867f9ee085cb2d2ee8722bc69aeab
Quimzy <bam.boy2020@gmail.com>
2023-03-05 22:22:54 +0300

    testing: tensor backward test

--- tests/tensor_test.py
+++ tests/tensor_test.py
@@ -1,5 +1,56 @@
 import sys
 sys.path.insert(1, '..')
 import numpy as np
+import torch
 from engine import Tensor
 
+X = np.random.rand(3,3)
+print(f'x data: \n{X}\n')
+Y = np.random.rand(1,3)
+print(f'y data: \n{Y}\n')
+
+def test_backward():
+    def braingrad():
+        print("\n\n******* BRAINGRAD *******\n\n")
+        x = Tensor(X)
+        y = Tensor(Y)
+
+        m = y.dot(x)
+        
+        v = y.logsoftmax().sum()
+
+        v.backward()
+
+        print(f'm data: \n{m.data}]\n')
+        z = m.sum()
+        print(f'z data: \n{z.data}\n')
+        z.backward()
+
+        print(f'x grad: \n{x.grad}\n')  # dz/dx
+        print(f'y grad: \n{y.grad}')  # dz/dy
+    
+        return x.grad, y.grad
+    
+    def pytorch():
+        print("\n\n******* TORCH *******\n\n")
+        x = torch.tensor(X, requires_grad=True)
+        y = torch.tensor(Y, requires_grad=True)
+
+        m = y.matmul(x)
+        
+        v = torch.nn.functional.log_softmax(y, dim=1).sum()
+
+        v.backward()
+
+        print(f'm data: \n{m.data}]\n')
+        z = m.sum()
+        print(f'z data: \n{z.data}\n')
+        z.backward()
+
+        print(f'x grad: \n{x.grad}\n')  # dz/dx
+        print(f'y grad: \n{y.grad}')  # dz/dy
+    
+        return x.grad, y.grad
+    
+    for a,b in zip(braingrad(), pytorch()):
+        np.testing.assert_allclose(a, b, atol=1e-6)
